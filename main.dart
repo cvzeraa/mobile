4700d31231f2a366aebb039424397d7aaa6649d3
cvzeraa <165086449+cvzeraa@users.noreply.github.com>
2025-09-04 00:47:47 +0000

    feat: if statement

--- main.dart
+++ main.dart
@@ -1,10 +1,22 @@
 import 'dart:io';
 void main (){
-    int idade = 18;
-    if(idade >= 18)
-{
-    print('pode entrar na balada');
+    double renda = 62000;
+    double saldo = 500000;
+
+    if(renda > 60000 && saldo > 100000){
+        print("exclusive");
+    }
+    else if(renda > 30000){
+        print("select");
+    }
+    else if(renda > 5000){
+        print("vangogh");
+    }
+    else if(renda > 1400){
+        print("Varejo");
+    }else{
+        print("Sem classificação");
+    }
 }
-print('fim do programa');
-}
+    
 
